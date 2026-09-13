62387068e008fedfee986e694677bdc90c975bf5
lenangson <Son.LN2417019@sis.hust.edu.vn>
2026-09-13 19:13:45 +0700

    13-09: Complete Architecture Trap_Handler; full trap_handler not yet implemented.

--- kernel/uart/trap.c
+++ kernel/uart/trap.c
@@ -1,17 +1,72 @@
-#include <stdint.h>
+#include<uart.h>
+#include<trap.h>
+#include<stdint.h>
 
 extern void trap_entry(void);
 
-void trap_init(void){
-    __asm__ volatile(
-        "la t0, %[trap_entry_addr]\n\t"
-        "csrrs zero, mtvec, t0"
+static inline unsigned long read_mcause(void){
+    unsigned long value;
+
+    asm volatile(
+        "csrr %[value], mcause"
+        : [value] "=r"(value)
         : 
-        : [trap_entry_addr] "r" (trap_entry)
         :
+    );
+    return value;
+}
+
+void machine_software_interrupt_handler(void){
+    // do something
+    uart_puts("Machine Software Interrupt");
+    *CLINT_BASE = 0;
+}
+
+void machine_timer_interrupt_handler(void){
+    // do something
+    uart_puts("Machine Timer Interrupt");
+    *CLINT_MTIMECMP = *CLINT_MTIME + INTERVAL;
+}
+
+void machine_external_interrupt_handler(void){
+    // do something
+    uart_puts("Machine External Interrupt");
+}
+
+void trap_init(void){
+    asm volatile(
+        "csrw mtvec, %0"
+        :
+        : "r"(trap_entry)
     );
 }
 
-void trap_handle(void){
-    
+void trap_handler(void){
+     unsigned long mcause;
+     unsigned long exception_code;
+     mcause = read_mcause();
+     exception_code = (mcause << 1) >> 1;
+     // interrupt
+     if (mcause >> 63){
+        switch(exception_code){
+            case 3:                // Machine (mode) Software Interrupt
+                machine_software_interrupt_handler();
+                break;
+            case 7:                // Machine (mode) Timer Interrupt
+                machine_timer_interrupt_handler();
+                break;
+            case 11:               // Machine (mode) External Interrupt
+                machine_external_interrupt_handler();
+                break;   
+            default:
+                uart_puts("Unknown interrupt");
+                break;
+
+        }
+     }
+     // exception
+     else {
+        // implement sau ở Ver_1 chỉ in ra thông tin về exception
+        uart_puts("Exception");
+     }
 }
--- kernel/uart/trap.h
+++ kernel/uart/trap.h
@@ -1,7 +1,12 @@
 #ifndef TRAP_H
 #define TRAP_H
 
+#define CLINT_BASE (volatile uint64_t*)0x02000000
+#define CLINT_MTIMECMP  (volatile uint64_t*)(CLINT_BASE+0x4000)
+#define CLINT_MTIME (volatile uint64_t*)(CLINT_BASE+0xBFF8)
+#define INTERVAL 10000
+
 void trap_init(void);
-void trap_handle(void);
+void trap_handler(void);
 
 #endif
